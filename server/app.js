df63a62bdd770fd569f89a78b26aaa59256e0622
gabrielalmeidasantos <gabriel.almeiidda@gmail.com>
2023-03-02 19:20:47 -0300

    autenticacao criada

--- server/app.js
+++ server/app.js
@@ -1,19 +1,122 @@
 require("dotenv").config();
-const app = require("express")();
+const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const port = 8080;
+const modelUsuario = require("./models/Usuario");
 
 const db_user = process.env.DB_USER;
 const db_pass = process.env.DB_PASS;
 
-app.get("/", (req, res) => {
-  res.status(200).json({ mensagem: "Ta funfando a rota raiz" });
+app.use(express.json());
+
+async function logado(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  let token = authHeader && authHeader.split(" "[1]);
+  token = token.toString();
+
+  if (!token) {
+    res.status(401).json({ msg: "acesso negado" });
+  }
+
+  try {
+    const secret = process.env.SECRET;
+    jwt.verify(token, secret);
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ msg: "Token invalido" });
+  }
+}
+
+app.get("/usuario/:id", logado, async (req, res) => {
+  const id = req.params.id;
+  const usuarioExiste = await modelUsuario.findById(id, "-senha");
+
+  if (!usuarioExiste) {
+    return res.status(422).json({ msg: "Email já cadastrado" });
+  }
+
+  return res.status(200).json(usuarioExiste);
 });
 
 app.post("/auth/cadastrar", async (req, res) => {
   const { nome, email, senha } = req.body;
+
+  if (!nome) {
+    return res.status(422).json({ msg: "Preencher o campo nome" });
+  }
+
+  if (!email) {
+    return res.status(422).json({ msg: "Preencher o campo email" });
+  }
+
+  if (!senha) {
+    return res.status(422).json({ msg: "Preencher o campo senha" });
+  }
+
+  const usuarioExiste = await modelUsuario.findOne({ email: email });
+
+  if (usuarioExiste) {
+    return res.status(422).json({ msg: "Email já cadastrado" });
+  }
+
+  const salt = await bcrypt.genSalt(12);
+  const novaSenha = await bcrypt.hash(senha, salt);
+
+  const usuario = new modelUsuario({
+    nome,
+    email,
+    senha: novaSenha,
+  });
+  try {
+    await usuario.save();
+    return res.status(201).json({ msg: "Usuario criado com sucesso" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ msg: "Erro no servidor" });
+  }
+});
+
+app.post("/auth/entrar", async (req, res) => {
+  const { email, senha } = req.body;
+
+  if (!email) {
+    return res.status(422).json({ msg: "Preencher o campo email" });
+  }
+
+  if (!senha) {
+    return res.status(422).json({ msg: "Preencher o campo senha" });
+  }
+
+  const usuarioExiste = await modelUsuario.findOne({ email: email });
+
+  if (!usuarioExiste) {
+    return res.status(422).json({ msg: "Usuario nao existe" });
+  }
+
+  const senhaCorreta = await bcrypt.compare(senha, usuarioExiste.senha);
+
+  if (!senhaCorreta) {
+    return res.status(422).json({ msg: "Senha incorreta" });
+  }
+
+  try {
+    const secret = process.env.SECRET;
+    const token = jwt.sign(
+      {
+        id: usuarioExiste._id,
+      },
+      secret
+    );
+
+    res.status(200).json({ token });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ msg: "Erro no servidor" });
+  }
 });
 
 mongoose
